b47d4abd5a6a29cad80435253e477b566621cf9e
RajaGitHub27 <48967517+RajaGitHub27@users.noreply.github.com>
2020-05-17 14:50:41 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,26 @@
+package com.app.test;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.app.model.Product;
+import com.app.util.HibernateUtil;
+
+public class Test {
+
+	public static void main(String[] args) {
+		try(Session ses = HibernateUtil.getSf().openSession()) {
+			Transaction tx = ses.beginTransaction();
+			String sql = "from com.app.model.Product where productId=:id";
+			Query query = ses.createQuery(sql);
+			query.setParameter("id", 7777);
+			Product product = (Product) query.uniqueResult();
+			ses.delete(product);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}//main
+
+}//class
